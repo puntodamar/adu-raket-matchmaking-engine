27482628d2fbf6471593d01f71d6967b5b012b23
puntodamar <punto.damar2@gmail.com>
2026-09-22 22:59:15 +0700

    update: create player ui based on skill level

--- app/matchmaking/[id]/player/page.tsx
+++ app/matchmaking/[id]/player/page.tsx
@@ -1,44 +1,37 @@
 'use client';
 
 import Heading from "@/components/Heading";
-import {Field, FieldLabel,} from "@/components/ui/field";
-import React, {useState} from "react";
-import {SKILL_LEVELS, SkillLevel, useSessionStore} from "@/src/store/useSessionStore";
-import {Input} from "@/components/ui/input";
+import { Field, FieldLabel } from "@/components/ui/field";
+import React, { useState } from "react";
+import { SKILL_LEVELS, SkillLevel, useSessionStore } from "@/src/store/useSessionStore";
+import { Input } from "@/components/ui/input";
 import {
     Select,
     SelectContent,
     SelectGroup,
     SelectItem,
-    SelectLabel,
     SelectTrigger,
     SelectValue,
-} from "@/components/ui/select"
-import {UserRoundPlus} from "lucide-react";
-import { Button } from "@/components/ui/button"
+} from "@/components/ui/select";
+import { UserRoundPlus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function CreatePlayer() {
     let currentSession = useSessionStore((state) => state.currentSession);
-    
+    const buttonClass = "hover:cursor-pointer hover:bg-accent flex flex-row gap-x-2 items-center justify-center p-2 rounded-lg bg-coral text-white text-sm hover:text-white";
+
+    const router = useRouter();
     const [playerName, setPlayerName] = useState('');
     const [playerLevel, setPlayerLevel] = useState<SkillLevel>('Pemula');
-    const fruits = [
-        { value: "1", label: "Pemula" },
-        { value: "2", label: "Pra-Menengah" },
-        { value: "3", label: "Menengah" },
-        { value: "4", label: "Menegah-Atas" },
-        { value: "5", label: "Pro" },
-    ];
-    const [selectedFruit, setSelectedFruit] = useState<string | null>("banana");
-    const handleSubmit = () => {
-    
+
+    const handleSubmit = (e: React.FormEvent) => {
     }
-    
+
     return (
         <div className="flex flex-1 flex-col">
             <div className="flex flex-1 flex-col items-center justify-center">
                 <Heading>Tambah Pemain</Heading>
-                {/* Added flex flex-col so gap-y-4 works between Fields */}
                 <form onSubmit={handleSubmit} className="mt-5 flex flex-col gap-y-4 w-full max-w-xs">
                     <Field className="mx-auto flex w-full flex-col gap-y-1">
                         <FieldLabel
@@ -46,7 +39,7 @@ export default function CreatePlayer() {
                             className="w-full justify-center text-center font-semibold">
                             Nama
                         </FieldLabel>
-                        
+
                         <Input
                             id="input-player-name"
                             type="text"
@@ -56,31 +49,29 @@ export default function CreatePlayer() {
                             className="input-field"
                         />
                     </Field>
-                    
+
                     <Field className="mx-auto flex w-full flex-col gap-y-1">
                         <FieldLabel
                             htmlFor="select-player-level"
                             className="w-full justify-center text-center font-semibold">
                             Level Bermain
                         </FieldLabel>
-                        
+
                         <div className="relative w-full">
-                            <Select value={selectedFruit} onValueChange={setSelectedFruit}>
-                                {/* Added text-center and w-full to the trigger */}
+                            <Select value={playerLevel} onValueChange={(val) => setPlayerLevel(val as SkillLevel)}>
                                 <SelectTrigger className="w-full cursor-pointer bg-white justify-center text-center" showIcon={false}>
-                                    <SelectValue  className="w-full text-center" >
-                                        {fruits.find((fruit) => fruit.value === selectedFruit)?.label}
+                                    <SelectValue className="w-full text-center">
+                                        {playerLevel}
                                     </SelectValue>
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectGroup>
-                                        {fruits.map((fruit) => (
+                                        {SKILL_LEVELS.map((level) => (
                                             <SelectItem
-                                                key={fruit.value}
-                                                value={fruit.value}
-                                                className="text-xs font-medium hover:bg-accent/50 hover:text-white data-[highlighted]:bg-accent/50 data-[highlighted]:text-white"
-                                            >
-                                                {fruit.label}
+                                                key={level}
+                                                value={level}
+                                                className="text-xs font-medium hover:bg-accent/50 hover:text-white data-[highlighted]:bg-accent/50 data-[highlighted]:text-white">
+                                                {level}
                                             </SelectItem>
                                         ))}
                                     </SelectGroup>
@@ -88,15 +79,19 @@ export default function CreatePlayer() {
                             </Select>
                         </div>
                     </Field>
-                    
-                    
-                    
-                    <Button className={'hover:cursor-pointer hover:bg-accent mt-2 bg-primary flex flex-row gap-x-2 items-center justify-center p-2 rounded-lg text-white text-sm'}>
-                        <UserRoundPlus data-icon="inline-start" size={16}  /> Tambah
-                    </Button>
+
+                    <div className="flex flex-row items-stretch gap-x-2 pt-3 mt-3 border-t border-coral/50">
+                        <Button type="button" onClick={() => router.back()} className={buttonClass} variant="outline">
+                            <span className="inline-flex items-center gap-x-2">
+                                <span>Kembali</span>
+                            </span>
+                        </Button>
+                        <Button type="submit" className="flex-1 hover:cursor-pointer hover:bg-accent bg-primary flex flex-row gap-x-2 items-center justify-center p-2 rounded-lg text-white text-sm">
+                            <UserRoundPlus data-icon="inline-start" size={16} /> Tambah
+                        </Button>
+                    </div>
                 </form>
             </div>
         </div>
-    
-    )
+    );
 }
